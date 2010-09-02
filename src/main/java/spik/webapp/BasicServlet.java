b072b04987b38b5820c393efb1d597c40921a74e
Alistair Jones <alistair.jones@gmail.com>
2010-09-02 15:39:13 +0100

    Do some renaming and simplify servlet

--- src/main/java/spik/webapp/BasicServlet.java
+++ src/main/java/spik/webapp/BasicServlet.java
@@ -7,28 +7,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
 
-/**
- * Created by IntelliJ IDEA.
- * User: sgreenha
- * Date: Sep 2, 2010
- * Time: 12:25:56 PM
- * To change this template use File | Settings | File Templates.
- */
 public class BasicServlet extends HttpServlet{
 
     private String message = "Hello from a basic servlet";
 
-    public BasicServlet(){}
-    public BasicServlet(String message) {
-        this.message = message;
-    }
-    
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        response.setContentType("text/html");
-        response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().println("<h1>"+message+"</h1>");
-        response.getWriter().println("<h3>Current Time:</h3> " + new Date(System.currentTimeMillis()));
+        response.setContentType("text/plain");
+        response.getWriter().println(message);
     }
 }
 
