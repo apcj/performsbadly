0459c6604a61870d55cf831ec785f44d89d6c066
James Bull <jbull@thoughtworks.com>
2010-09-09 20:06:32 +0100

    Added leaky servlet. Not added it to app yet.

---
+++ src/main/java/performsbadly/webapp/MemoryLeakServlet.java
@@ -0,0 +1,43 @@
+package performsbadly.webapp;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jbull
+ * Date: Sep 9, 2010
+ * Time: 7:53:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+
+public class MemoryLeakServlet {
+    private String message;
+    private List<String> importantCollectionOfStuff;
+    private String sillyString;
+    private static final int LEAKYNESS = 100;
+
+    public MemoryLeakServlet() {
+        this.message = "Mmmmmn Leaky";
+        importantCollectionOfStuff = new ArrayList<String>();
+        sillyString = "This is a silly string.";
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LeakMemory(LEAKYNESS);
+        response.setContentType("text/plain");
+        response.getWriter().println(message);
+    }
+
+    private void LeakMemory(int leakyness) {
+        for(int x = 0 ; x < leakyness ; x++){
+           importantCollectionOfStuff.add(sillyString);
+        }
+    }
+}
