830e3adc744544abb0c1c1c6a00bde03e27f88d9
Alistair Jones <alistair.jones@gmail.com>
2010-09-09 23:31:56 +0100

    Choose more appropirate module name

--- src/main/java/performsbadly/webapp/MemoryLeakServlet.java
+++ src/main/java/performsbadly/webapp/MemoryLeakServlet.java
@@ -8,21 +8,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by IntelliJ IDEA.
- * User: jbull
- * Date: Sep 9, 2010
- * Time: 7:53:08 PM
- * To change this template use File | Settings | File Templates.
- */
-
-
-
 public class MemoryLeakServlet extends HttpServlet {
     private String message;
     private List<String> importantCollectionOfStuff;
     private String sillyString;
-    private static final int LEAKYNESS = 100;
+    private static final int LEAKINESS = 100;
 
     public MemoryLeakServlet() {
         this.message = "Gruyere packed and dispatched";
@@ -31,13 +21,13 @@ public class MemoryLeakServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        LeakMemory(LEAKYNESS);
+        LeakMemory(LEAKINESS);
         response.setContentType("text/plain");
         response.getWriter().println(message);
     }
 
-    private void LeakMemory(int leakyness) {
-        for(int x = 0 ; x < leakyness ; x++){
+    private void LeakMemory(int leakiness) {
+        for(int x = 0 ; x < leakiness ; x++){
            importantCollectionOfStuff.add(sillyString);
         }
     }
